80a4529f416e05a29a69ce473d95d2a6f557750f
ksevenCh <92891752+ksevenCh@users.noreply.github.com>
2025-05-14 23:14:05 +0300

    Add: second neural network

---
+++ cnn2.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import time
+
+import tensorflow as tf
+from tensorflow import keras
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.model_selection import train_test_split
+
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
+from keras.utils import to_categorical
+from keras.models import Sequential
+
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout, BatchNormalization, GlobalAveragePooling2D
+from keras.optimizers import Adam
+
+train_path = 'dataset/train'
+test_path = 'dataset/test'
+
+height = 224
+width = 224
+
+batch_size = 32 # потом 64
+seed = 42
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   validation_split=0.2)
+train_dataset = train_datagen.flow_from_directory(train_path,
+                                                  target_size=(height, width),
+                                                  batch_size=batch_size,
+                                                  class_mode='categorical',
+                                                  shuffle=True,
+                                                  seed=seed,
+                                                  color_mode='rgb',
+                                                  interpolation='hamming',
+                                                  subset='training')
+
+test_datagen = ImageDataGenerator(rescale=1./255,
+                                  validation_split=0.2)
+test_dataset = test_datagen.flow_from_directory(test_path,
+                                                target_size=(height, width),
+                                                batch_size=batch_size,
+                                                class_mode='categorical',
+                                                shuffle=True,
+                                                seed=seed,
+                                                color_mode='rgb',
+                                                interpolation='hamming',
+                                                subset='validation')
+
+model = Sequential([    
+    Conv2D(filters=16, kernel_size=(5,5), activation='relu', input_shape=(height,width,3)),
+    Conv2D(filters=32, kernel_size=(5,5), activation='relu'),
+    MaxPool2D(pool_size=(2, 2)),
+    BatchNormalization(axis=-1),
+    
+    Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    MaxPool2D(pool_size=(2, 2)),
+    BatchNormalization(axis=-1),
+    Dropout(rate=0.25),
+
+    GlobalAveragePooling2D(),
+    Dense(512, activation='relu'),
+    BatchNormalization(),
+    Dropout(rate=0.25),
+    
+    Dense(8, activation='softmax')
+])
+
+alpha=0.01
+epochs=15
+optim = Adam(learning_rate=0.01)
+model.compile(optimizer = optim, loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+early_stopping = keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=3, restore_best_weights=True)
+
+start_time = time.time()
+
+cnn = model.fit(train_dataset,
+                steps_per_epoch=len(train_dataset),
+                epochs=epochs,
+                validation_data=test_dataset,
+                validation_steps=len(test_dataset),
+                callbacks=[early_stopping]
+)
+
+end_time = time.time()
+training_time = end_time - start_time
+
+mins, secs = divmod(training_time, 60)
+print(f"\nВремя обучения: {int(mins)} мин {int(secs)} сек")
+
+model.save('models/model3')
+
+loss, accuracy = model.evaluate(test_dataset)
+print(f"Test Accuracy: {accuracy:.4f}")
+
+fig,ax=plt.subplots(1,2)
+fig.set_size_inches(16,12)
+performance = pd.DataFrame(cnn.history)
+plt.figure(figsize=(10,7))
+ax[1].plot(performance[['loss','val_loss']])
+ax[1].legend(['Training Loss', 'Validation Loss'])
+ax[1].set_title('Model Loss')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Loss')
+ax[1].set_ylim(0,1)
+ax[0].plot(performance[['accuracy','val_accuracy']])
+ax[0].legend(['Training Accuracy', 'Validation Accuracy'])
+ax[0].set_title('Model Accuracy')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_ylim(0.5,1)
+fig.suptitle('CNN Performance')
+plt.show()
